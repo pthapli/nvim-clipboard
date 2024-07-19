31fbb1987c102b880883f5fc829fc4a7e100a840
pthapligg <pthapli@nostacklabs.com>
2024-07-19 23:30:06 +0530

    added functionality to open buffer to view list of strings at the center of currently opened buffer

--- lua/nvim-clipboard.lua
+++ lua/nvim-clipboard.lua
@@ -1,42 +1,93 @@
--- M = {}
--- local vim = vim
-
--- -- Define a function to monitor clipboard changes
--- M.monitor_clipboard = function()
--- 	local clipboard_text = vim.fn.getreg("*") -- Get the content of the system clipboard
--- 	vim.api.nvim_out_write("Clipboard changed: " .. clipboard_text .. "\n") -- Log the change to the screen
-
--- 	local clipboard_text_1 = vim.fn.getreg("+") -- Get the content of the system clipboard
--- 	vim.api.nvim_out_write("Clipboard changed 1: " .. clipboard_text_1 .. "\n") -- Log the change to the screen
--- end
-
--- -- Set up an autocmd to trigger the monitor_clipboard function when the clipboard changes
--- vim.cmd([[
---     augroup ClipboardMonitor
---         autocmd!
---         autocmd TextYankPost * lua require'nvim-clipboard'.monitor_clipboard()
---     augroup END
--- ]])
-
--- vim.api.nvim_out_write("Clipboard monitoring plugin loaded.\n")
-
--- M.Mister = function()
--- 	print("MISTER PT")
--- end
-
--- vim.api.nvim_out_write("Clipboard monitoring plugin loaded.\n")
-
--- return M
-
 M = {}
+M.clipboard_history = {}
+
 local vim = vim
 local last_clipboard_content = "" -- Variable to store the last clipboard content
 
+function M.show_list(items)
+	-- Create a new buffer
+	local buf = vim.api.nvim_create_buf(false, true)
+
+	-- Populate the buffer
+	vim.api.nvim_buf_set_lines(buf, 0, -1, true, items)
+
+	local current_win = vim.api.nvim_get_current_win()
+	local win_width = vim.api.nvim_win_get_width(current_win)
+	local win_height = vim.api.nvim_win_get_height(current_win)
+
+	local new_win_width = 80 -- Adjust as needed
+	local new_win_height = 20 -- Adjust as needed
+
+	local row = math.floor(win_height / 2 - new_win_height / 2)
+	local col = math.floor(win_width / 2 - new_win_width / 2)
+
+	local options = {
+		relative = "win",
+		width = new_win_width,
+		height = new_win_height,
+		row = row,
+		col = col,
+		border = "single",
+	}
+
+	local win = vim.api.nvim_open_win(buf, true, options)
+
+	-- Set up keymaps for navigation and selection
+	vim.keymap.set("n", "<up>", function()
+		vim.cmd("norm k")
+	end, { buffer = buf })
+	vim.keymap.set("n", "<down>", function()
+		vim.cmd("norm j")
+	end, { buffer = buf })
+	vim.keymap.set("n", "<esc>", function()
+		vim.cmd("close")
+	end, { buffer = buf })
+	vim.keymap.set("n", "<cr>", function()
+		local selected_item = vim.api.nvim_buf_get_lines(
+			buf,
+			vim.api.nvim_win_get_cursor(win)[1] - 1,
+			vim.api.nvim_win_get_cursor(win)[1] - 1,
+			true
+		)[1]
+		-- Do something with the selected item
+		print(selected_item)
+		vim.cmd("close")
+	end, { buffer = buf })
+end
+
+local function write_to_file(text)
+	local file = io.open("clipboard.txt", "a")
+	if file ~= nil then
+		file:write(text .. "\n")
+		file:close()
+	else
+		print("File is nil")
+	end
+end
+
+local function read_from_file()
+	local file = io.open("clipboard.txt", "r")
+	if file ~= nil then
+		for line in file:lines() do
+			table.insert(M.clipboard_history, line)
+		end
+		file:close()
+	else
+		print("File is nil")
+	end
+end
+
+local your_list = { "hello", "mister", "dj", "mera" }
+vim.keymap.set("n", "<leader>b", function()
+	require("nvim-clipboard").show_list(your_list)
+end)
 -- Define a function to monitor clipboard changes
 M.monitor_clipboard = function()
 	local clipboard_text = vim.fn.getreg("+") -- Get the content of the system clipboard
 	if clipboard_text ~= last_clipboard_content then -- If the clipboard content has changed
-		vim.api.nvim_out_write("Clipboard changed: " .. clipboard_text .. "\n") -- Log the change to the screen
+		table.insert(M.clipboard_history, clipboard_text)
+		write_to_file(clipboard_text)
+		vim.api.nvim_out_write("Clipboard changed bero: " .. clipboard_text .. "\n") -- Log the change to the screen
 		last_clipboard_content = clipboard_text -- Update the last clipboard content
 	end
 end
